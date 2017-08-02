ed42428dc8d3347fac63018be3fbb6862d42e9f4
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2017-08-02 15:24:04 -0400

    add bubble sort algorithm

---
+++ bubble_sort.py
@@ -0,0 +1,24 @@
+def bubble_sort(l):
+    lc = l[::]
+
+    def swap(i, j):
+        aux = lc[i]
+        lc[i] = lc[j]
+        lc[j] = aux
+
+    for i in range(len(lc)-1):
+        for j in range(len(lc)-1):
+            print("({} < {})".format(lc[j+1], lc[j]))
+            if lc[j+1] < lc[j]:
+                print("({}: {} <- {}: {})".format(j, lc[j], j+1, lc[j+1]))
+                swap(j, j+1)
+
+        print(lc)
+        print("--")
+    return lc
+
+l = [10,9,8,7,6,5,4,3,2,1]
+print(l)
+new = bubble_sort(l)
+
+print("from {} to {}".format(l, new))
